c7a4ea76749c15fb287804d401750d9049bd9e4b
esperantkela <esperant.kela@gmail.com>
2023-11-07 14:25:13 +0100

    feat:retrive product

--- App.js
+++ App.js
@@ -1,17 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Switch, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Switch, Text, View } from "react-native";
 import { useColorScheme } from "nativewind";
 
 export default function App() {
-
   const { colorScheme, setColorScheme } = useColorScheme();
   return (
-    <View className="flex-1 items-center justify-center bg-gray-200 dark:bg-black"> 
-      <Switch value={colorScheme === "dark"} onChange={setColorScheme}/>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View className="flex-1 items-center justify-center bg-gray-200 dark:bg-black">
+      <View className="flex-row w-full gap-5">
+        <Text className="text-2xl font-bold dark:text-white ">
+         New collection
+        </Text>
+        <Switch value={colorScheme === "dark"} onChange={setColorScheme} />
+      </View>
+        <StatusBar style={colorScheme === 'dark' ? 'light' : 'dark'}/>
     </View>
   );
 }
-
-
