5ac1674f4e8ae551b3f99711f4df0cc204a51a4d
esperantkela <esperant.kela@gmail.com>
2023-11-07 16:54:20 +0100

    feat:create product card

--- components/ProductList.js
+++ components/ProductList.js
@@ -1,11 +1,13 @@
 import { FlatList, Text, View } from 'react-native'
 import React from 'react'
 import { products } from '../products'
+import ProductCard from './ProductCard'
 
 const ProductList = () => {
   return (
     <FlatList data={products} keyExtractor={(product) => product.id}
-        renderItem={({item}) => <Text>{item.title}</Text>}
+        renderItem={({item}) => <ProductCard {...item}/>}
+        contentContainerStyle={{ paddingHorizontal : 15 }}
     />
   )
 }
