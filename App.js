657c9f2d53abd0fc5ac239e96436badc7ee2e04e
esperantkela <esperant.kela@gmail.com>
2023-11-07 17:02:15 +0100

    change dark mode

--- App.js
+++ App.js
@@ -1,4 +1,3 @@
-import { StatusBar } from "expo-status-bar";
 import { SafeAreaView, Switch, Text, View } from "react-native";
 import { useColorScheme } from "nativewind";
 import ProductList from "./components/ProductList";
@@ -7,14 +6,13 @@ export default function App() {
   const { colorScheme, toggleColorScheme } = useColorScheme();
   return (
     <SafeAreaView className="flex-1 items-center justify-center bg-gray-200 dark:bg-black">
-      <View className="flex-row w-full gap-5">
+      <View className="flex-row justify-between w-full gap-5">
         <Text className="text-2xl font-bold dark:text-white ">
          New collection
         </Text>
         <Switch value={colorScheme === "dark"} onChange={toggleColorScheme} />
       </View>
       <ProductList/>
-        <StatusBar style={colorScheme === 'dark' ? 'light' : 'dark'}/>
     </SafeAreaView>
   );
 }
